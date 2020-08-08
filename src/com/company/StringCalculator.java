64d548e6b46da2aae1052087f8904ab076174321
Ryan Lin <ruyanglin@gmail.com>
2020-08-07 23:51:14 -0400

    working but needs more test

---
+++ src/com/company/StringCalculator.java
@@ -0,0 +1,82 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class StringCalculator {
+
+
+    private List<String> delimiters = new ArrayList<>();
+    private List<Integer> negatives  = new ArrayList<>();
+    private final int LOWER_BOUND = 0;
+    private final int UPPER_BOUND = 1000;
+    private final String NORMALIZED_DELIMITER = ",";
+
+
+    public int add(String str) throws Exception {
+        List<Integer> numbers = parseString(str);
+
+        if (!negatives.isEmpty()) {
+            throw new Exception("Negatives not allowed: " + negatives);
+        }
+
+        return numbers.stream().reduce(0, Integer::sum);
+    }
+
+    public List<String> getDelimiters() {
+        if (delimiters.size() == 0) {
+            return Collections.singletonList(",");
+        }
+        return delimiters;
+    }
+
+
+    private List<Integer> parseString(String str) {
+        String[] tokens;
+        List<Integer> numbers = new ArrayList<>();
+
+        if (str.length() == 0) {
+            return numbers;
+        }
+
+        if (str.charAt(0) == str.charAt(1) && str.charAt(0) == '/') {
+            String[] formatedString = str.substring(2).split("\n", 2);
+            str = formatedString[1];
+            delimiters = Arrays.asList(formatedString[0].split(NORMALIZED_DELIMITER));
+
+            for (String delim: delimiters) {
+                // Replace doesn't take delimiter as a regex
+                str = str.replace(delim, NORMALIZED_DELIMITER);
+            }
+        }
+        tokens = str.split(NORMALIZED_DELIMITER);
+
+        // Clean and filter the numbers
+        for (String s: tokens) {
+            String cleaned = clean(s);
+
+            // For cased where the delimiters are * and **
+            int number = cleaned.equals("") ? 0 : Integer.parseInt(cleaned);
+            if (isAcceptable(number))  {
+                numbers.add(number);
+            }
+        }
+
+        return numbers;
+    }
+
+    private String clean(String str) {
+        return str.replaceAll("\n", "");
+    }
+
+    private boolean isAcceptable(int number) {
+        if (number < 0) {
+            negatives.add(number);
+        }
+
+        return number >= LOWER_BOUND && number <= UPPER_BOUND;
+    }
+
+}
---
+++ test/StringCalculatorTest.java
@@ -0,0 +1,105 @@
+import com.company.StringCalculator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StringCalculatorTest {
+
+    StringCalculator stringCalc = new StringCalculator();
+
+    @Test
+    public void BasicTest() throws Exception {
+        String input = "1,2,3";
+        assertEquals(stringCalc.add(input), 6);
+    }
+
+    @Test
+    public void EmptyTest() throws Exception {
+        String input = "";
+        assertEquals(stringCalc.add(input), 0);
+    }
+
+    @Test
+    public void BasicWithSingleTest() throws Exception {
+        String input = "1,";
+        assertEquals(stringCalc.add(input), 1);
+    }
+
+    @Test
+    public void BasicWithNewLineTest() throws Exception {
+        String input = "1,\n2,4\n";
+        assertEquals(stringCalc.add(input), 7);
+    }
+
+    @Test
+    public void CustomDelimiterBasicTest() throws Exception {
+        String input = "//@\n1@2@4";
+        assertEquals(stringCalc.add(input), 7);
+    }
+
+    @Test
+    public void CustomDelimiterWithNewlineTest() throws Exception {
+        String input = "//;\n4;2\n;\n\n9";
+        assertEquals(stringCalc.add(input), 15);
+    }
+
+    @Test
+    public void CustomDelimiterLongTest() throws Exception {
+        String input = "//;[]72#k^^*!&\n4;[]72#k^^*!&2\n;[]72#k^^*!&\n\n9";
+        assertEquals(stringCalc.add(input), 15);
+    }
+
+    @Test(expected = Exception.class)
+    public void NegativeNumberExceptionTest() throws Exception {
+        String input = "-1,2,3";
+        stringCalc.add(input);
+    }
+
+    @Test
+    public void BonusOneBasicTest() throws Exception {
+        String input = "//%\n1001%100";
+        assertEquals(stringCalc.add(input), 100);
+    }
+
+    @Test
+    public void BonusOneLowerBoundaryTest() throws Exception {
+        String input = "//%\n10%0%3";
+        assertEquals(stringCalc.add(input), 13);
+    }
+
+    @Test
+    public void BonusOneUpperBoundaryTest() throws Exception {
+        String input = "//%\n1000%1001%3";
+        assertEquals(stringCalc.add(input), 1003);
+    }
+
+    @Test
+    public void BonusOneBothBoundaryTest() throws Exception {
+        String input = "//%\n1000%0%3";
+        assertEquals(stringCalc.add(input), 1003);
+    }
+
+    @Test
+    public void BonusTwoArbitaryLengthTest() throws Exception {
+        String input = "//***\n1***2\n***3\n";
+        assertEquals(stringCalc.add(input), 6);
+    }
+
+    @Test
+    public void BonusThreeMultipleDelimiterTest() throws Exception {
+        String input = "//*,&\n10*21\n&13\n";
+        assertEquals(stringCalc.add(input), 44);
+    }
+
+    @Test
+    public void BonusThreeMultipleSimilarDelimiterTest() throws Exception {
+        String input = "//*,**\n10*21\n**13\n";
+        assertEquals(stringCalc.add(input), 44);
+    }
+
+    @Test
+    public void BonusFourMultipleLengthDelimiterTest() throws Exception {
+        String input = "//*&&,*!**!*\n10*!**!*21\n*&&13*!**!*1\n";
+        assertEquals(stringCalc.add(input), 45);
+    }
+}
